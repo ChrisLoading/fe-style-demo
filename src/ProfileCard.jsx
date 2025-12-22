f897bf6398e1875d71370c03da95842c9f69cf6f
Hao <tsunghaochou@gmail.com>
2025-12-22 17:06:26 +0800

    fix HTML bug and add an avatar

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
+import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 
 export default function ProfileCard() {
     const [online, setOnline] = useState(true);
 
     return (
         <div className="pc-card">
-            <div className="pc-avatar">
+            <div className="pc-avatar" style={{ backgroundImage: `url(${reactLogo})` }}></div>
                 <div>
                     <h3 className="pc-title">John Doe</h3>
-                    <p className="pc-subtitle">MIS</p>
+                    <p className="pc-subtitle">Internal Systems / MIS</p>
 
                     <button className="pc-btn" onClick={() => setOnline(!online)}>
                         Toggle Online
                     </button>
                 </div>
-            </div>
             
             <span className={`pc-badge ${online ? "online" : "offline"}`}>
                 {online ? "Online" : "Offline"}
