9be73c455bbbd93f244d6e093adb78e85692ad0c
Hao <tsunghaochou@gmail.com>
2025-12-23 13:12:51 +0800

    style: remove comment

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 
 export default function ProfileCard() {
     const [online, setOnline] = useState(true);
