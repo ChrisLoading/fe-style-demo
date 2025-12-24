f02d5b6e0078136fb99af16c391f78d3f362e7e3
Hao <tsunghaochou@gmail.com>
2025-12-24 11:10:19 +0800

    chore: import tailwind stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 
 // import './index.css'  // Default styles
 import './styles/global.css'  // Global styles
+import './indexTailwind.css'  // Tailwind CSS styles (for clean Tailwind styles in App.jsx)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
